c81b3e0564543bc8d36d1ac19e66cbfd800a8c88
RishabhShrm02 <143181129+RishabhShrm02@users.noreply.github.com>
2024-07-15 23:35:50 +0530

    attendence management system

---
+++ data.js
@@ -0,0 +1,13 @@
+export const noOfClasses = {
+    "total_classes": 250,
+    "ML": 50,
+    "OOSD": 50,
+    "DBMS": 50,
+    "DAA": 50,
+    "WT": 50
+};
+
+//TODO: make a formula to calculate attendance percentage 
+
+
+
